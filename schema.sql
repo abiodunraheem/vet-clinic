ccb733c60d76b6c108b0070caf395661c74dc436
abiodunraheem <raheemabiodun23@gmail.com>
2022-07-14 16:31:10 +0100

    Create species and owners tables

--- schema.sql
+++ schema.sql
@@ -9,3 +9,27 @@ CREATE TABLE animals (
     weight_kg DECIMAL NOT NULL
 );
 ALTER TABLE animals ADD species VARCHAR(100); 
+
+-- Create owners table
+CREATE TABLE owners (
+id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+full_name VARCHAR(250) NOT NULL,
+age INT NOT NULL
+);
+
+-- Create species table
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250) NOT NULL
+);
+
+-- Remove column species
+ALTER TABLE animals DROP species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT species_id FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT owner_id FOREIGN KEY(owner_id) REFERENCES owners(id);
