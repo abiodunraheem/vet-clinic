933f595a0ef0764b429e9efa7ee28ca3dcc8d520
abiodunraheem <raheemabiodun23@gmail.com>
2022-07-20 12:47:33 +0100

    Updat database for a clinic

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,7 +13,7 @@ CREATE DATABASE clinic;
     CREATE TABLE medical_histories ( 
         id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         admitted_at TIMESTAMP,
-        patient_id INTEGER,
+        patient_id INT,
         status VARCHAR(20),
         CONSTRAINT fk_patient_id 
             FOREIGN KEY (patient_id) 
@@ -31,14 +31,6 @@ CREATE DATABASE clinic;
         id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         medical_history_id INT,
         treatment_id INT,
-        CONSTRAINT fk_medical_history_id 
-            FOREIGN KEY (medical_history_id) 
-            REFERENCES medical_histories(id) 
-            ON DELETE CASCADE,
-        CONSTRAINT fk_treatment_id 
-            FOREIGN KEY (treatment_id) 
-            REFERENCES treatments(id) 
-            ON DELETE CASCADE
     );
 
     CREATE TABLE invoices ( 
@@ -46,13 +38,7 @@ CREATE DATABASE clinic;
         total_amount DECIMAL,
         generated_at TIMESTAMP,
         payed_at TIMESTAMP,
-        medical_history_id INTEGER,
-        CONSTRAINT unique_history_id 
-                UNIQUE(medical_history_id),
-        CONSTRAINT fk_medical_history_id 
-            FOREIGN KEY (medical_history_id) 
-            REFERENCES medical_histories(id) 
-            ON DELETE CASCADE
+        medical_history_id INT,
     );
 
      CREATE TABLE invoice_items ( 
@@ -60,14 +46,6 @@ CREATE DATABASE clinic;
         unit_price DECIMAL,
         quantity INT,
         total_price DECIMAL,
-        invoice_id INTEGER,
-        treatment_id INTEGER,
-        CONSTRAINT fk_invoices_id 
-            FOREIGN KEY (invoice_id) 
-            REFERENCES invoices(id) 
-            ON DELETE CASCADE,
-        CONSTRAINT fk_treatment_id 
-            FOREIGN KEY (treatment_id) 
-            REFERENCES treatments(id) 
-            ON DELETE CASCADE
+        invoice_id INT,
+        treatment_id INT,
     );
